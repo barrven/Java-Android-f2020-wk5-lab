db605abbf5a4789e7f2a02d4d142dedeb6d5ec60
Barrington <barrven@users.noreply.github.com>
2020-10-09 14:08:38 -0400

    finished up to map working with search box and search button with listener and animation

--- app/src/main/java/ca/gbc/comp3074/java_android_f2020_wk5_lab/MapsActivity.java
+++ app/src/main/java/ca/gbc/comp3074/java_android_f2020_wk5_lab/MapsActivity.java
@@ -2,7 +2,12 @@ package ca.gbc.comp3074.java_android_f2020_wk5_lab;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -11,18 +16,51 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    EditText locationNameText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.main_layout);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        locationNameText = findViewById(R.id.location_text);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = locationNameText.getText().toString();
+                if(!name.isEmpty()){
+                    Geocoder geocoder = new Geocoder(getApplicationContext());
+                    //translate locations into addresses list
+                    try {
+                        List<Address> addresses = geocoder.getFromLocationName(name, 1);
+                        if(addresses != null && !addresses.isEmpty()){
+                            Address a = addresses.get(0);
+                            LatLng pos = new LatLng(a.getLatitude(), a.getLongitude());
+                            if(mMap != null){
+                                mMap.addMarker(new MarkerOptions().position(pos).title(name));
+                                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(pos, 17));
+                            }
+
+                        }
+                    }
+                    catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
     }
 
 
@@ -40,8 +78,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        //change the map marker to Casa Loma Campus
+        LatLng clc = new LatLng(43.675987,-79.410797);
+        mMap.addMarker(new MarkerOptions().position(clc).title("Marker in GBC Casa Loma"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(clc, 17)); //change method to zoom and pass zoom level 17
     }
 }

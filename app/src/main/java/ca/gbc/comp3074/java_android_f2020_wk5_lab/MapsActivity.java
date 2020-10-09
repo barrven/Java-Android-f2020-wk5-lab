48ad597ab307e8164b10361cd0aa5c4a8f27b9f9
Barrington <barrven@users.noreply.github.com>
2020-10-09 15:30:59 -0400

    completed lab code

--- app/src/main/java/ca/gbc/comp3074/java_android_f2020_wk5_lab/MapsActivity.java
+++ app/src/main/java/ca/gbc/comp3074/java_android_f2020_wk5_lab/MapsActivity.java
@@ -1,10 +1,21 @@
 package ca.gbc.comp3074.java_android_f2020_wk5_lab;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,7 +30,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import java.io.IOException;
 import java.util.List;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 
     private GoogleMap mMap;
     EditText locationNameText;
@@ -61,8 +72,72 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 }
             }
         });
+
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
+            //we have permission from the user to get location
+            getLocation();
+        }
+        else{
+            //asks the system to ask user to grant location permission to this app
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+
+
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1
+                && permissions.length == 1
+                && permissions[0] == Manifest.permission.ACCESS_FINE_LOCATION
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+            //then request can be processed cause permissions are valid. have permission to access the location
+            getLocation();
+        }
+    }
+
+    void getLocation(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
+            //we have permission from the user to get location
+            LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+            if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+                Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                if (location != null){
+                    LatLng pos = new LatLng(location.getLatitude(), location.getLongitude());
+                    if (mMap != null){
+                        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(pos, 18));
+                    }
+                }
+
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,100, 1, this);
+            }
+            else{
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        assert locationManager != null;
+        locationManager.removeUpdates(this);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 100, 1, this);
+        }
+    }
 
     /**
      * Manipulates the map once available.
@@ -87,5 +162,30 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng clc = new LatLng(43.675987,-79.410797);
         mMap.addMarker(new MarkerOptions().position(clc).title("Marker in GBC Casa Loma"));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(clc, 17)); //change method to zoom and pass zoom level 17
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if (location != null){
+            LatLng pos = new LatLng(location.getLatitude(), location.getLongitude());
+            if (mMap != null){
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(pos, 18));
+            }
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
